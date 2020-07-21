610d83081493db8037de97e58e67c35c7e61cbf5
root <root@localhost.localdomain>
2020-07-21 16:07:59 +0800

    random port

--- pkt.gen.py
+++ pkt.gen.py
@@ -3,8 +3,8 @@ import random
 
 #logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 
-strLoad = os.urandom(random.randint(64, 65535))
+strLoad = os.urandom(random.randint(64, 1000))
 
-res = [sendp(Ether()/IP(dst=str(RandIP()))/TCP()/Raw(load=strLoad), iface='enp1s0f1', verbose=False) for x in range(0, 10000)]
+#res = [sendp(Ether()/IP(dst=str(RandIP()))/TCP()/Raw(load=strLoad), iface='enp1s0f1', verbose=False) for x in range(0, 10000)]
+res = [sendp(Ether()/IP()/TCP(dport=random.randrange(1000, 60000))/Raw(load=strLoad), iface='eth1', verbose=False) for x in range(0, 1000)]
 exit()
-
